2b92e51fd181c462c10f729b2e874dc86c54c173
0leksandrr <tsybulskyy.sasha@gmail.com>
2025-01-05 14:39:40 +0200

    frontend: added a nested folder react_js

--- frontend/react_js/src/App.jsx
+++ frontend/react_js/src/App.jsx
@@ -3,7 +3,6 @@ import Dashboard from './pages/Dashboard';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import { AuthProvider } from "./context/AuthContext";
-import PrivateRoute from './components/PrivateRoute';
 import './styles/global.css';
 
 
